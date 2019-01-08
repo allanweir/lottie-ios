3755fb17572c9637ac66208a585cb8f0e513c0c5
Allan Weir <allan.weir@me.com>
2019-01-08 12:06:17 +0000

    Enable debugging

--- lottie-ios/Classes/Extensions/LOTHelpers.h
+++ lottie-ios/Classes/Extensions/LOTHelpers.h
@@ -13,8 +13,8 @@
 #import "CGGeometry+LOTAdditions.h"
 #import "LOTBezierPath.h"
 
-#define ENABLE_DEBUG_LOGGING NO
-#define ENABLE_DEBUG_SHAPES NO
+#define ENABLE_DEBUG_LOGGING YES
+#define ENABLE_DEBUG_SHAPES YES
 
 #endif /* LOTHelpers_h */
 
